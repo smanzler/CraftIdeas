e495bc6ff22ef0d82c42c890a682f7701250424c
smanzler <simanzler@gmail.com>
2025-01-30 15:38:19 -0500

    added more cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import Card from "./components/Card";
 
 function App() {
   return (
-    <>
-      <Card />
-    </>
+    <div className="cards-container">
+      <Card src="src\assets\ae0ff820b7d2738b668f2d7150b180cd.jpg" />
+      <Card src="src\assets\hq720.jpg" />
+      <Card src="src\assets\689478c349fc264cda42ea24626fe441.jpg" />
+    </div>
   );
 }
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,9 @@
 import "../css/Card.css";
 
-function Card() {
+function Card({ src }: { src: string }) {
   return (
     <div className="card-container">
-      <img
-        className="card-image"
-        src="src\assets\ae0ff820b7d2738b668f2d7150b180cd.jpg"
-        alt="Minecraft Build Thumbnail"
-      />
+      <img className="card-image" src={src} alt="Minecraft Build Thumbnail" />
       <div className="card-overlay">
         <div className="overlay-buttons">
           <button>View Details</button>
